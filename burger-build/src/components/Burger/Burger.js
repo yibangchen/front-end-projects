2fd70a82f7202631d2d702bc1c126010beec71db
Yibang Chen <ychen622@gmail.com>
2018-03-14 21:31:43 -0700

    add state

--- burger-build/src/components/Burger/Burger.js
+++ burger-build/src/components/Burger/Burger.js
@@ -5,16 +5,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-	const formedIngredients = Object.keys(props.ingredients)
+	let formedIngredients = Object.keys(props.ingredients)
 		.map(ing => {
 			return [...Array(props.ingredients[ing])].map((_, i) => {
 				return <BurgerIngredient key={ing+i} type={ing} />
 			});
-		});
+		})	// array of array --> flattened array
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, []);
+
+	if (formedIngredients.length === 0) {
+		formedIngredients = <p>Please start adding ingredients!</p>
+	}
 
 	return (
 		<div className={ classes.Burger }>
+			<BurgerIngredient type='bread-top' />
 			{formedIngredients}
+			<BurgerIngredient type='bread-bottom' />		
 		</div>
 	);
 }

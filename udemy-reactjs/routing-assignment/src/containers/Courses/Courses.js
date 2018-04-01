afffb918513f43964aa2eae0fdaf5017ee6ca623
Yibang Chen <ychen622@gmail.com>
2018-03-31 22:57:11 -0700

    routing project init

---
+++ udemy-reactjs/routing-assignment/src/containers/Courses/Courses.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import './Courses.css';
+import Course from '../Course/Course';
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+
+    clickHandler(id) {
+        console.log("clicked");
+        this.props.history.push({ pathname: this.props.match.url + '/' + id});
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        this.state.courses.map( course => {
+                        return (
+                            <div>
+                                <article 
+                                    className="Course" 
+                                    key={course.id}
+                                    onClick={ ()=> this.clickHandler(course.id) }
+                                >
+                                    {course.title}
+                                </article>
+                                <Route path={this.props.match.url + "/:id"} exact component={Course} />
+                            </div>
+                        );
+                        } )
+                    }
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Courses;
---
+++ udemy-reactjs/routing-assignment/src/containers/Users/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+class Users extends Component {
+    render () {
+        return (
+            <div>
+                <h1>The Users Page</h1>
+            </div>
+        );
+    }
+}
+
+export default Users;
